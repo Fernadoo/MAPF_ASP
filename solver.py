25f96118fabe5dacf1e33b09b6aa6fa0159456fd
mac_zhu <zfm19980618@icloud.com>
2022-01-22 18:19:56 +0800

    111

--- solver.py
+++ solver.py
@@ -30,9 +30,51 @@ class ASPSolver():
                 i = eval(name.split('p')[-1])
                 lp.write(f'goal{i}(G) :- G = {goal_config[name]}.\n')
 
-            # Global settings
+            # Global settings - global state formatting
             n = len(agent_config.keys())
-            
+            SLs = ', '.join([f'L{i}' for i in range(1, n + 1)])
+            SGs = ', '.join([f'G{i}' for i in range(1, n + 1)])
+            S = f'S = ({SLs}, {SGs})'
+            constraints = ''
+            goals = ''
+            Ls = ''
+            for i in range(1, n + 1):
+                for j in range(i + 1, n + 1):
+                    constraints += f'L{i} != L{j}, '
+                constraints += '\n'
+                goals += f'goal{i}(G{i}), '
+                Ls += f'L{i} = (X{i}, Y{i}), ' \
+                      f'cell(X{i}, Y{i}), ' \
+                      f'not block(X{i}, Y{i}),\n'
+            goals += '\n'
+            Ls = Ls[:-2] + '.'
+            lp.write('gState(S) :-' + S + constraints + goals + Ls)
+
+            # Global settings - transition
+            action_profile = ', '.join([f'A{i}' for i in range(1, n + 1)])
+            L1s = ', '.join([f'L1{i}' for i in range(1, n + 1)])
+            G1s = ', '.join([f'G1{i}' for i in range(1, n + 1)])
+            S1 = f'S1 = ({L1s}, {G1s}),\n'
+            L2s = ', '.join([f'L2{i}' for i in range(1, n + 1)])
+            G2s = ', '.join([f'G2{i}' for i in range(1, n + 1)])
+            S2 = f'S2 = ({L2s}, {G2s}),\n'
+            L1_decomp = ', '.join([f'L1{i} = (X1{i}, Y1{i})'
+                                   for i in range(1, n + 1)])
+            L2_decomp = ', '.join([f'L2{i} = (X2{i}, Y2{i})'
+                                   for i in range(1, n + 1)])
+            move = ',\n'.join([f'move(X1{i}, Y1{i}, A{i}, X2{i}, Y2{i})'
+                               for i in range(1, n + 1)])
+            notswap = ''
+            for i in range(1, n + 1):
+                for j in range(i + 1, n + 1):
+                    pass
+
+
+
+
+
+
+
         os.system('cat examples/2agents_template.lp >> tmp.lp')
         return 'tmp.lp'
 
